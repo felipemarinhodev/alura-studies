f9b94cd0175e0d411e676adaf1aed633d198b173
felipemarinhodev <felipmarinho@gmail.com>
2022-03-03 06:46:48 -0800

    move and add style to App

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import Formulario from '../components/Formulario';
+import List from '../components/List';
+
+import './style.scss'
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario />
+
+      <List />
+    </div>
+  );
+}
+
+export default App;
